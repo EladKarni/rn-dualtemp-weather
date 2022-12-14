943872723fef4366aa6729df9b0b6a03e3d26306
Elad Karni <elad.karni@gmail.com>
2022-12-14 15:31:43 -0500

    Fix: Updated cli to use tunnel

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, Platform, StatusBar } from 'react-native'
 import { WeatherIconStyles } from '../WeatherIcon/WeatherIcon.Styles';
 import { typography } from '../../Styles/Typography';
 
@@ -20,26 +20,26 @@ const AppHeader = ({ location }: AppHeaderPropTypes) => {
 }
 
 const styles = StyleSheet.create({
-    headerContainer: {
-
-    },
-    containerHeaderText: {
-      fontSize: 20,
-      textAlign: 'center',
-    },
-    locationText: {
-      paddingRight: 5
-    },
-    locationHeader: {
-      margin: 'auto',
-      height: 30,
-      paddingBottom: 5,
-      marginBottom: 15,
-      paddingHorizontal: 10,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
+  headerContainer: {
+    paddingTop: 1
+  },
+  containerHeaderText: {
+    fontSize: 20,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    textAlign: 'center',
+  },
+  locationText: {
+    paddingRight: 5,
+  },
+  locationHeader: {
+    margin: 'auto',
+    height: 30,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
   });
 
 export default AppHeader
