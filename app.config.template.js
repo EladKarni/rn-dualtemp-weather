6aafd5387a954d1b2506c07a94b7f8a48366fcad
Elad Karni <elad.karni@brandingbrand.com>
2023-01-08 04:34:55 -0500

    Fix: Config Template

--- app.config.template.js
+++ app.config.template.js
@@ -4,7 +4,7 @@ export default ({ config }) => {
       weatherAPI: process.env.OPEN_WEATHER_API ?? "",
       revGeoAPI: process.env.REVERSE_GEO_API ?? "",
       eas: {
-        projectId: "444bda66-1ab4-4665-ba53-c2b76743a33b"
+        projectId: ""
       }
     };
   
