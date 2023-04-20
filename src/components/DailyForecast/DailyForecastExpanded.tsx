28d6d325651a3088aa9964adc1104e10260777ed
Elad Karni <elad.karni@gmail.com>
2023-04-20 10:15:42 -0400

    Mom Update (her suggestion lol)

--- src/components/DailyForecast/DailyForecastExpanded.tsx
+++ src/components/DailyForecast/DailyForecastExpanded.tsx
@@ -92,7 +92,7 @@ const DailyForecastExpanded = ({ day }: DailyForecastItemExpandedPropTypes) => {
 
                     <Text style={DailyForecastExtendedItemStyles.infoFeelTitle}>Feels Like</Text>
                     <DailyExpandedFeelInfo temp={day.feels_like.morn} label={'Morn'} />
-                    <DailyExpandedFeelInfo temp={day.feels_like.day} label={'Day'} />
+                    <DailyExpandedFeelInfo temp={day.feels_like.day} label={'Noon'} />
                     <DailyExpandedFeelInfo temp={day.feels_like.eve} label={'Even'} />
                     <DailyExpandedFeelInfo temp={day.feels_like.night} label={'Night'} />
 
