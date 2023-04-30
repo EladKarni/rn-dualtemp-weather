1aa0749f31cb593b37d839008ac0ccc7303b21ae
Elad Karni <39969481+EladKarni@users.noreply.github.com>
2023-04-30 16:49:02 +0000

    Updated color names and usage

--- src/Styles/Palette.ts
+++ src/Styles/Palette.ts
@@ -1,8 +1,8 @@
 export const palette = {
-  textColor: '#0a2344',
-  highlightColor: '#d66ba0',
-  primaryColor: '#f686bd',
-  primaryLight: '#f4bbd3',
-  primaryDark: '#fe5d9f',
-  shadowLight: '#d6d2d2',
+  textColor: '#fff',
+  highlightColor: '#EAEAF3',
+  primaryColor: '#3621DC',
+  primaryLight: '#6B58FF',
+  primaryDark: '#1C1B4D',
+  shadowLight: '#715ef5',
 };
