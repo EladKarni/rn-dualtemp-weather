45de0f16d44cbee41e4c5a007ff912c617119eb3
Elad Karni <elad.karni@brandingbrand.com>
2023-05-02 20:25:56 -0400

    changed endpoints to backend proxy

--- src/utils/fetchReverseGeocoding.ts
+++ src/utils/fetchReverseGeocoding.ts
@@ -1,14 +1,11 @@
-
-import Constants from 'expo-constants';
-
-const geoApiKey: string = Constants.expoConfig?.extra ? Constants.expoConfig?.extra.revGeoAPI : ''
+import { base_url } from "./fetchWeather";
 
 const requestOptions = {
   method: 'GET',
 };
 
 export const fetchReverseGeocoding = async (lat: number, long: number) => {
-    return fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=${geoApiKey}`, requestOptions)
+    return fetch(`${base_url}get-location?lat=${lat}&long=${long}`, requestOptions)
         .then(response => response.json())
         .then(result => result.features[0].properties)
         .catch(error => console.log('error', error));
