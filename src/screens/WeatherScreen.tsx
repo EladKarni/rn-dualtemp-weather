03c2aa81a4d692edd5b0be64b3fcb83b0a330e61
Elad Karni <elad.karni@gmail.com>
2026-01-04 15:01:56 -0500

    fix: update WeatherScreen styles to use paddingTop instead of marginTop

--- src/screens/WeatherScreen.tsx
+++ src/screens/WeatherScreen.tsx
@@ -111,7 +111,7 @@ export default function WeatherScreen({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 10,
+    paddingTop: 10,
     backgroundColor: palette.primaryDark,
   },
 });
